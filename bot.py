18ea0b28b6526001135286744b1b58fa130ed24e
erikahutieva <erikahutieva16@gmail.com>
2024-10-30 13:31:06 +0300

    done

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from pydub.generators import Sine
 import telebot
 from MukeshAPI import api
 from gtts import gTTS
+import threading
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -15,6 +16,23 @@ bot = telebot.TeleBot(TOKEN)
 START_BTNS = [
     ["Генерировать изображение", "generate_image"],
 ]
+
+# Flag for bot status
+running = True
+
+# Polling in a separate thread
+def start_polling():
+    global running
+    while running:
+        try:
+            bot.polling(none_stop=True)
+        except Exception as e:
+            print(f"Ошибка при запуске: {e}")
+            bot.stop_polling()
+
+# Start polling in a thread
+polling_thread = threading.Thread(target=start_polling)
+polling_thread.start()
 
 # Функция генерации изображения
 def gen_img(msg):
@@ -65,6 +83,14 @@ def gen_audio_handler(msg):
 def repo_handler(msg):
     bot.send_message(msg.chat.id, f"Вот ссылка на репозиторий: {REPO_URL}")
 
+# Обработчик команды /stop для остановки бота
+@bot.message_handler(commands=['stop'])
+def stop_handler(msg):
+    global running
+    bot.send_message(msg.chat.id, "Бот отключен.")
+    running = False
+    bot.stop_polling()  # Stop polling to break the loop
+
 # Обработчик текстовых сообщений
 @bot.message_handler(content_types=['text'])
 def text_handler(msg):
@@ -81,6 +107,3 @@ def callback_handler(call):
     if call.data == "generate_image":
         bot.send_message(call.message.chat.id, "Отправьте текст для генерации изображения.")
         bot.register_next_step_handler(call.message, gen_img)
-
-# Запуск бота
-bot.polling(none_stop=True)
